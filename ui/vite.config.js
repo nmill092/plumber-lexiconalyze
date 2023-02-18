e1b0c269fe732d9f41b683cf8abdf144be487182
Reed Miller <nile@techimpact.org>
2023-02-17 19:27:37 -0700

    reset cache

--- ui/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/plumber-lexiconalyze/",
-  plugins: [svelte()],
-})
