f20a422c46e8b6692bcd9964b7827ea649177419
Reed Miller <nile@techimpact.org>
2023-02-18 14:00:42 -0700

    change intro wording

--- ui/src/components/Intro.svelte
+++ ui/src/components/Intro.svelte
@@ -9,7 +9,7 @@
         This is a simple tool that helps you evaluate the sentiment of one or more pieces of text using <a class="font-bold text-indigo-700 decoration-0" href="https://www.knime.com/blog/lexicon-based-sentiment-analysis" target="_blank" rel="noreferrer">lexicon-based sentiment analysis</a>. 
     </p>
 
-    <p class="mt-4"><span class="font-bold">How it works:</span> First, we clean your texts by removing common filler words, numbers, and punctuation. Then we use your chosen sentiment lexicon to categorize each word of your text. You'll then see the results visualized in a chart and a table.</p>
+    <p class="mt-4"><span class="font-bold">How it works:</span> First, the app cleans your text by removing common filler words, numbers, and punctuation, and reducing words to their simplest forms. Then it uses your chosen sentiment lexicon to categorize each word of your text. You'll then see the results visualized in a chart, table, and word cloud.</p>
 
     <p class="mt-4">Get started by pasting your texts into the section below.</p>
 
